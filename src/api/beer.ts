8982a08b7de344e65cbcf2457f6ec5d8fda7dc54
Lisnyi <russelhobbbs@gmail.com>
2023-07-16 23:41:47 +0300

    Update beer.ts

--- src/api/beer.ts
+++ src/api/beer.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { Beer } from '../types'; 
 
 const BASE_URL = 'https://api.punkapi.com';
 
@@ -6,7 +7,7 @@ const instance = axios.create({
   baseURL: BASE_URL,
 });
 
-export const getBeers = async (page: number) => {
+export const getBeers = async (page: number):Promise<Beer[]> => {
     const { data } = await instance.get(`/v2/beers?page=${page}`);
     return data;
 };
